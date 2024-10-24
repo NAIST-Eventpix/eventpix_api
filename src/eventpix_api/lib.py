53e7e1b89e82d5c99e639fa168cfd1a05a114e75
take0x <89313929+take0x@users.noreply.github.com>
2024-10-24 22:23:25 +0900

    Add CI workflow for checking codes

--- src/eventpix_api/lib.py
+++ src/eventpix_api/lib.py
@@ -1,8 +1,10 @@
 import io
+from typing import Any
 
 from PIL import Image
 
-def pick_schedule_from_image(image_data: bytes):
+
+def pick_schedule_from_image(image_data: bytes) -> dict[str, Any]:
     try:
         image = Image.open(io.BytesIO(image_data))
 
--- src/eventpix_api/main.py
+++ src/eventpix_api/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 
-from src.eventpix_api.lib import pick_schedule_from_image
+from eventpix_api.lib import pick_schedule_from_image
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
+
 @app.post("/pick_schedule_from_image")
-async def api_pick_schedule_from_image(file: UploadFile = File(...)):
+async def api_pick_schedule_from_image(file: UploadFile = File(...)) -> JSONResponse:
     try:
         contents = await file.read()
         return JSONResponse(content=pick_schedule_from_image(contents))
